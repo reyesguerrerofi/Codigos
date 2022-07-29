577389dc8dfc4ef3ad57c41dd2b5cfb4cb1a743f
reyesguerrerofi <antonio1122235@gmail.com>
2022-07-28 20:05:53 -0500

    Commit 3: Variables y constantes

---
+++ Java/variables.java
@@ -0,0 +1,21 @@
+package Java;
+/*Codigo operadores y variables */
+public class variables {
+  public static void main(String args[]){
+    //!Este es un comentario JAVA
+    
+    int a=5,b=7;
+    int c = b + a;
+
+    c++; //*Incrementa en uno el valor de c
+    System.out.println(c);
+    c+=3; //*Se incrementa en 3
+    System.out.println(c);
+    c--;//*Disminuye en 1
+
+    final int d = 4; //*Esta es una constante
+
+    System.out.println(d);
+
+  }
+}
